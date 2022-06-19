18b10ec062580b3c78a14a0d3ff73d0562ad4d2f
Donie Kelly <donie.kelly@gmail.com>
2022-06-20 00:50:29 +0100

    Updated comments

--- app/Main.hs
+++ app/Main.hs
@@ -41,7 +41,7 @@ runController :: Chan Event -> ReaderT Config (StateT Controller IO) ()
 runController chan = forever $ do
   event <- liftIO $ readChan chan
   case event of
-    TickEvent   -> do liftIO (putStr "tick"); return ()
+    TickEvent   -> do liftIO (putStr "tick"); return () -- here we update the graphics as requried
     KeyEvent k  -> do
       case k of
         'a' -> armController
@@ -74,7 +74,7 @@ armController = do
 --    let (x, y) = axis controller
 
 -- Joystic center position (50%, 50%) is no trottle and mid steering
--- Joystick left and right control the truster position which
+-- Joystick left and right control the truster positions which
 -- rotate around their center positions by a configurabele limit
 type Axis         = Int
 newtype Joystick  = Joystick { getPosition :: (Axis, Axis)} deriving Show
@@ -96,7 +96,8 @@ data Controller   = Controller
       trusterPower   :: PowerLevel, -- Truster power level 0-100%
       batteryLevel   :: Battery,    -- Current battery percentage
       batteryLowLevel:: Battery,    -- Low battery level warning level - truster enters low power mode
-      batteryShutdown:: Battery     -- Battery shutdown level, disarms controller
+      batteryShutdown:: Battery,    -- Battery shutdown level, disarms controller
+      updateUI       :: Bool        -- If state is updated then this must be set to true to update UI
    }
 
 initController :: IO Controller
@@ -104,13 +105,14 @@ initController = do
    timeNow <- getSystemTime
    return $ Controller
       {
-         axis              = Joystick (50, 50),
-         armed             = Off,
-         lastArmed         = timeNow,
+         axis              = Joystick (50, 50), -- centered
+         armed             = Off,               -- controller off
+         lastArmed         = timeNow,           -- can't arm within two seconds from now
          trusterPower      = 0,
          batteryLevel      = 100,
          batteryLowLevel   = 10,
-         batteryShutdown   = 2
+         batteryShutdown   = 2,
+         updateUI          = False
       }
 
 data Config       = Config
