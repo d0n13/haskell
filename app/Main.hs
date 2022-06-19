0385f3138c19656c8e29e9af8500de7d6d98b69c
Donie Kelly <donie.kelly@gmail.com>
2022-06-19 23:27:57 +0100

    Added more code around setup

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,14 @@
 module Main where
 
 import System.IO
---import System.Console.ANSI
+import Data.Time
 import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
 import Control.Concurrent 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewState (CommandConfig)
+import Data.Time.Clock.System (SystemTime (MkSystemTime), getSystemTime)
 
 --- Main
 main :: IO ()
@@ -21,18 +22,57 @@ main = do
 
 --main = animate window azure (draw)
 
+-- Generate a tick event every 50ms to get 20fps
 castTick :: Chan Event -> IO ()
 castTick chan = forever $ do
   threadDelay (50 ^ 4)
   writeChan chan TickEvent
 
--- State of control trusters
+-- Read the keyboard
+castKey :: Chan Event -> IO ()
+castKey chan = forever $ do
+  hSetEcho stdin False
+  c <- getChar
+  writeChan chan (KeyEvent c)
+
+-- Controller loop
+runController :: Chan Event -> ReaderT Config (StateT Controller IO) ()
+runController chan = forever $ do
+  event <- liftIO $ readChan chan
+  case event of
+    TickEvent   -> do liftIO (putStr "tick"); return ()
+    KeyEvent k  -> do
+      case k of
+        'a' -> armController
+      --   'w' -> moveJoystick U
+      --   's' -> moveJoystick D
+      --   'j' -> moveJoystick L
+      --   'k' -> moveJoystick R
+        _  -> return ()
+
+-- Arm or disarm the controller
+-- Only allows state to change once per seconds -- handle time difference between last press and now
+armController :: MonadState Controller m => m ()
+armController = do
+   controller <- get
+   let isArmed = armed controller 
+   let timePressed = getSystemTime
+   put $ controller {armed = isArmed, lastArmed = timePressed}
+   return ()
+
+-- moveJoystick :: MonadState Controller m => JoystickMove -> m ()
+-- moveJoystick direction = do 
+--    contoller <- get
+--    let (x, y) = axis controller
+
+
 
 -- Joystic center position (50%, 50%) is no trottle and mid steering
 -- Joystick left and right control the truster position which
 -- rotate around their center positions by a configurabele limit
 type Axis         = Int
 newtype Joystick  = Joystick { getPosition :: (Axis, Axis)} deriving Show
+data JoystickMove = U | D | L | R deriving Show
 data Armed        = On | Off deriving Show
 type Battery      = Int
 type PowerLevel   = Int
@@ -44,26 +84,33 @@ data Event
 
 data Controller   = Controller 
    {
-      axis     :: Joystick,
-      armed    :: Armed,
-      power    :: PowerLevel,
-      batLevel :: Battery
+      axis           :: Joystick,   -- Joystick axis (x,y) 0-100% 50% is center
+      armed          :: Armed,      -- On off switch, inputs have no response when not armed
+      lastArmed      :: SystemTime, -- Time the arm button was last pressed. Arm or disarm must be debounced
+      trusterPower   :: PowerLevel, -- Truster power level 0-100%
+      batteryLevel   :: Battery,    -- Current battery percentage
+      batteryLowLevel:: Battery,    -- Low battery level warning level
+      batteryShutdown:: Battery     -- Battery shutdown level, disarms controller
    }
-   deriving Show
 
 initController :: IO Controller
-initController = return $ Controller
-  {
-    axis       = Joystick (50, 50),
-    armed      = Off,
-    power      = 0,
-    batLevel   = 100
-  }
+initController = do
+   timeNow <- getSystemTime 
+   return $ Controller
+      {
+         axis              = Joystick (50, 50),
+         armed             = Off,
+         lastArmed         = timeNow,
+         trusterPower      = 0,
+         batteryLevel      = 100,
+         batteryLowLevel   = 10,
+         batteryShutdown   = 2
+      }
 
 data Config       = Config
    {
-      trusterMaxTurn :: Int,
-      powerLimit     :: PowerLimit
+      trusterMaxTurn :: Int,        -- Maximum amoutn truster can turn left or right
+      powerLimit     :: PowerLimit  -- Max amount of power available (low, med, high)
    }
    deriving Show
 
